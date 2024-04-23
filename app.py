02602b21a003e2c331514d3e753ae0538f6b380d
Andrés Méndez <andres.mendez.solano@est.una.ac.cr>
2024-04-22 21:28:24 -0600

    Endpoint para cliente del prototipo del profe

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,3 +11,45 @@ def hello_world():
 @app.route('/test')
 def test():
     return 'Test from flask API'
+
+
+# Ruta para manejar la petición de EnvioDatos
+@app.route('/envio-datos', methods=['POST'])
+def envio_datos():
+    # Recuperar los datos enviados en la petición POST
+    idlog = request.form['idlog']
+    tr = request.form['tr']
+    ta = request.form['ta']
+    tc = request.form['tc']
+    tb = request.form['tb']
+    tv = request.form['tv']
+    hr = request.form['hr']
+    ha = request.form['ha']
+    hc = request.form['hc']
+    hb = request.form['hb']
+    hv = request.form['hv']
+    ca = request.form['ca']
+    ne = request.form['ne']
+    le = request.form['le']
+    uv = request.form['uv']
+    # Add to dictionary to print
+    data = {
+        'idlog': idlog,
+        'tr': tr,
+        'ta': ta,
+        'tc': tc,
+        'tb': tb,
+        'tv': tv,
+        'hr': hr,
+        'ha': ha,
+        'hc': hc,
+        'hb': hb,
+        'hv': hv,
+        'ca': ca,
+        'ne': ne,
+        'le': le,
+        'uv': uv
+    }
+    print('Datos recibidos: ', data)
+    # return 200 and message Datos recibidos
+    return 'Datos recibidos'
