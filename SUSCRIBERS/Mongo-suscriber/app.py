939265fb57dc774b261713e7bf9a7fae39b8db4a
Andrés Méndez <andres.mendez.solano@est.una.ac.cr>
2024-05-08 19:06:15 -0600

    first again

--- SUSCRIBERS/Mongo-suscriber/app.py
+++ SUSCRIBERS/Mongo-suscriber/app.py
@@ -1,38 +1,19 @@
 '''
 autor: Andrés Méndez Solano
-fecha: 2020-04-22
+fecha: 2020-05-08
     
         Este script se encarga de levantar un servidor web con Flask y de definir las rutas para recibir los datos de dispositivos IoT
 '''
 
 # app.py
 from flask import Flask, request
-from database import insert_data
 
 app = Flask(__name__)
 
-@app.route('/publishers', methods=['POST'])
-def envio_datos():
-    data = request.get_json()
-    print('Datos recibidos -> ', data)
-    publisher_name = data['publisher_name']
-    timestamp = data['data'].pop('timestamp', None)
-    save_to_db = data['save_to_db']
-    if save_to_db:
-        for key, value in data['data'].items():
-            attribute_data = {
-                'publisher_name': publisher_name,
-                'attribute_name': key,
-                'attribute_value': value,
-                'timestamp': timestamp
-            }
-            try:
-                insert_data(attribute_data)
-            except Exception as e:
-                print('Error -> ', e)
-                return 'Error saving in db', 500
-
-    return 'Datos recibidos', 200
+# hello world
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run(debug=True)
