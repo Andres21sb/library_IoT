5aeb478bfb59c53cb8447c13d6376753ae71358e
Andrés Méndez <andres.mendez.solano@est.una.ac.cr>
2024-05-08 19:14:29 -0600

    a

--- SUSCRIBERS/Mongo-suscriber/app.py
+++ SUSCRIBERS/Mongo-suscriber/app.py
@@ -2,15 +2,15 @@
 autor: Andrés Méndez Solano
 fecha: 2020-05-08
     
-        Este script se encarga de levantar un servidor web con Flask y de definir las rutas para recibir los datos de dispositivos IoT
+        Este script se encarga de levantar un servidor web con Flask y de guardar los datos que recibe en una base de datos de MongoDB
 '''
 
 # app.py
 from flask import Flask, request
 
-app = Flask(__name__)
 
-# hello world
+app = Flask(__name__)
+#hello world
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
